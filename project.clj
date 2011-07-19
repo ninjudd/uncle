2949d955082348f65c3479cf6876d91c095e7dac
Anthony Grimes <i@raynes.me>
2011-07-19 17:36:54 -0500

    Add cake-marginalia.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject uncle "0.2.3"
   :description "Lancet-inspired ant library for clojure."
   :dependencies [[clojure "1.2.0"]
-                 [org.apache.ant/ant "1.8.1"]])
+                 [org.apache.ant/ant "1.8.1"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :tasks [cake-marginalia.tasks])
